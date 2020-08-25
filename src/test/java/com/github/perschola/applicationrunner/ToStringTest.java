210d3107b97aa7a0b41911248b401e5d141c80c8
git-leon <xleonhunter@gmail.com>
2020-08-24 20:53:53 -0400

    update

--- src/test/java/com/github/perschola/applicationrunner/ToStringTest.java
+++ src/test/java/com/github/perschola/applicationrunner/ToStringTest.java
@@ -4,12 +4,10 @@ import com.github.perschola.ApplicationRunner;
 import com.github.perschola.itemcontainerinterface.ItemContainerInterface;
 import com.github.perschola.itemcontainerinterface.ShoppingCartService;
 import com.github.perschola.itemcontainerinterface.ShoppingStoreService;
-import com.github.perschola.model.Item;
 import com.github.perschola.model.ItemBuilder;
 import com.github.perschola.model.ItemInterface;
 import com.github.perschola.utils.RandomDataGenerator;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -42,8 +40,10 @@ public class ToStringTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    private List<ItemInterface> generateRandomStoreItems(int numberOfItems) {
-        return Stream
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3,4,10})
+    public void test(int numberOfItemsToBeAddedToStore) {
+        List<ItemInterface> itemsToBeAddedToStore = Stream
                 .generate(() -> {
                     int randomAvailableQuantity = randomDataGenerator.createInteger(1, 100);
                     int randomQuantity = randomDataGenerator.createInteger(1, randomAvailableQuantity);
@@ -56,14 +56,9 @@ public class ToStringTest {
                             .setItemPrice(randomPrice)
                             .build();
                 })
-                .limit(numberOfItems)
+                .limit(numberOfItemsToBeAddedToStore)
                 .collect(Collectors.toList());
-    }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0,1,2,3,4,10})
-    public void test(int numberOfItemsToBeAddedToStore) {
-        List<ItemInterface> itemsToBeAddedToStore = generateRandomStoreItems(numberOfItemsToBeAddedToStore);
         List<ItemInterface> itemsToBeAddedToCart = itemsToBeAddedToStore.subList(0, randomDataGenerator.createInteger(1, itemsToBeAddedToStore.size()));
         test(itemsToBeAddedToStore, itemsToBeAddedToCart);
     }
